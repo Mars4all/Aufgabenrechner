6f8a8da38e7475af7d04e95e15334e9acb6cf531
Mars4all <88582613+Mars4all@users.noreply.github.com>
2022-02-20 13:51:45 +0100

    hochgeladen, das ist ja super

---
+++ index.js
@@ -0,0 +1,44 @@
+window.addEventListener("load", function(){
+    
+
+
+        let button2 = document.getElementById("calculate2");
+        button2.addEventListener("click", function(){
+
+            let height2 = document.getElementById("height2");
+            let weight2 = document.getElementById("weight2");
+
+            //mit value habe ich den Rückgabewert
+            let bmi2 = (Math.sqrt((weight2.value * weight2.value) +  (height2.value *height2.value)));
+            
+            console.log(height2.value);
+            console.log(weight2.value);
+            alert (bmi2);
+
+        });
+
+        
+
+    });
+
+
+    window.addEventListener("load", function(){
+
+    let button2 = document.getElementById("calculate3");
+        button2.addEventListener("click", function(){
+
+            let height3 = document.getElementById("height3");
+            let weight3 = document.getElementById("weight3");
+
+            //mit value habe ich den Rückgabewert
+            let bmi3 = (weight3.value * weight3.value) - (height3.value * height3.value);
+            let bmi3n = Math.sqrt(bmi3);
+            
+            
+            console.log(height3.value);
+            console.log(weight3.value);
+            alert (bmi3n);
+
+        });
+
+    });
